d03964cde23f9330dc71b69c5aa2ef6486afa79c
Godwin <godwinolagunju0@gmail.com>
2022-10-21 02:12:54 -0700

    Adding week 7

--- Week 5/Class/out.py
+++ Week 5/Class/out.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "http://127.0.0.1:5000/predict"
+url = "http://127.0.0.1:9696/predict"
 data = {"customerid": "7892-pookp",
         "gender": "female",
         "seniorcitizen": 0,
