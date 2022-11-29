bc3ca76455fd5d8309879be6a59133a88b2f81bb
Godwin <godwintoluwani4@gmail.com>
2022-11-28 23:50:31 -0800

    modify lambdafunction

--- Week9/class/lambdafunction.py
+++ Week9/class/lambdafunction.py
@@ -1,26 +1,6 @@
 from PIL import Image
 import tflite_runtime.interpreter as tflite
 from keras_image_helper import create_preprocessor
-<<<<<<< HEAD:Week9/class/lambdafunction.py
-import numpy as np
-import tensorflow.lite as tflite
-
-#Classes
-classes = ['dress',
-            'hat',
-            'longsleeve',
-            'outwear',
-            'pants',
-            'shirt',
-            'shoes',
-            'shorts',
-            'skirt',
-            't-shirt']
-preprocessor  = create_preprocessor('xception', target_size=(299,299))
-
-interpreter = tflite.Interpreter(model_path = "./lite_model.tflite")
-interpreter.allocate_tensors()
-=======
 
 
 preprocessor = create_preprocessor('xception', target_size=(299, 299))
@@ -48,7 +28,6 @@ classes = [
 
 # url = 'http://bit.ly/mlbookcamp-pants'
 
->>>>>>> 639f374b3b7a2f057005017e111a9b9199ea3371:Week9/lambdafunction.py
 def preprocessing(path):
     with Image.open(path) as img:
         img = img.resize((299,299), Image.NEAREST)
@@ -61,16 +40,9 @@ def preprocessing(path):
 def predict(url):
     X = preprocessor.from_url(url)
 
-<<<<<<< HEAD:Week9/class/lambdafunction.py
-    img = preprocessor.from_url(url)
-    #img = preprocessing(img_path)
-    #interpreter = tflite.Interpreter(model_path = "C:/Users/Godwin/Documents/Workflow/MLZoomcamp/ML-ZoomCamp/Week9/lite_model.tflite")
-    #interpreter.allocate_tensors()
-=======
     interpreter.set_tensor(input_index, X)
     interpreter.invoke()
     preds = interpreter.get_tensor(output_index)
->>>>>>> 639f374b3b7a2f057005017e111a9b9199ea3371:Week9/lambdafunction.py
 
     float_predictions = preds[0].tolist()
 
